5b32ca401e7fcff083213e8326dca40ce5de7a4d
msi43089 <msi43089@gmail.com>
2021-07-11 14:35:06 -0400

    Starting Polybius Solution

--- src/polybius.js
+++ src/polybius.js
@@ -4,15 +4,44 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+  //define polybius square
+  const toEncode = {a: "11", b: "21", c: "31", d: "41", e: "51",
+                    f: "12", g: "22", h: "32", i: "42", j: "42", k: "52",
+                    l: "13", m: "23", n: "33", o: "43", p: "53",
+                    q: "14", r: "24", s: "34", t: "44", u: "54",
+                    v: "15", w: "25", x: "35", y: "45", z: "55", " ": " "
+                    }
+  const toDecode = {11: "a", 21: "b", 31: "c", 41: "d", 51: "e",
+                    12: "f", 22: "g", 32: "h", 42: "(i/j)", 52: "k",
+                    13: "l", 23: "m", 33: "n", 43: "o", 53: "p",
+                    14: "q", 24: "r", 34: "s", 44: "t", 54: "u",
+                    15: "v", 25: "w", 35: "x", 45: "y", 55: "z", " ": " "
+                    }
+  
 
+            
   function polybius(input, encode = true) {
-    // your solution code here
+    //convert input to lowercase
+    const lower = input.toLowerCase();
+    //convert input to array
+    const encoded = lower.split("")
+    //map corresponing object value to array
+    .map(letter => toEncode[letter])
+    //convert array into string 
+    .join("")
+    
+    
+
+
+   return encoded
   }
+
 
   return {
     polybius,
   };
 })();
+
+
 
 module.exports = { polybius: polybiusModule.polybius };
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,30 @@
-// Write your tests here!
+const polybiusModule = require("../src/polybius");
+const expect = require("chai").expect
+
+describe("polybius", () =>{
+    it("Should encode the input", () => {
+        const input = 'message'
+        const actual = polybiusModule.polybius(input);
+        const expected = "23513434112251"
+        expect(actual).to.equal(expected)
+
+    })
+    it("Should transale i/j both to 42", () => {
+        const input = 'i'
+        const altInput = 'j'
+        const actual = polybiusModule.polybius(input);
+        const altActual = polybiusModule.polybius(altInput);
+        const expected = "42"
+        expect(actual).to.equal(expected)
+        expect(altActual).to.equal(expected)
+        expect(actual).to.equal(altActual)
+    })
+    it("Should ignore capital letters", () => {
+        const input = 'CaPiTal'
+        const actual = polybiusModule.polybius(input);
+        const expected = "31115342441113"
+        expect(actual).to.equal(expected)
+
+    })
+
+})
