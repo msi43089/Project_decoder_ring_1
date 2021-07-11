2ccb83c61cc66d066178b9f7cf9bf8aa6258296d
msi43089 <msi43089@gmail.com>
2021-07-11 19:57:05 -0400

    Start Substitution Solution

--- src/substitution.js
+++ src/substitution.js
@@ -4,9 +4,29 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
+  let realAlphabet = "abcdefghijklmnopqrstuvwxyz"
+  let alpha = realAlphabet.split("")
+
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
+    //early return false if alphabet is missing or does not have 26 characters
+    if (!alphabet || alphabet.length !== 26) return false
+    //convert alphabet into an array
+    let subAlphabet = alphabet.split("");
+    //check for dupicate elements in the array - sort array
+    let sortAlpha = subAlphabet.sort()
+    //loop thru sorted array
+    for (let i = 0; i < sortAlpha.length; i++)
+      //if current indexed element equals next indexed element return false
+      if(sortAlpha[i +1] === sortAlpha[i]){
+        return false
+      }
+    let inputArray = input.split("")
+      //find the index of each "inputArray" element in the aplpha array
+      //find the element that corresponds to the same index in the "subAlphabet" array
+
+
     // your solution code here
   }
 
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,23 @@
-// Write your tests here!
+const substitutionModule = require("../src/substitution");
+const expect = require("chai").expect
+
+describe("substitution", () =>{
+    it("Should return false if duplicate value", () => {
+        const input = "message"
+        const alphabet = "xoyqmcgrukswaflnthdjpzibel"
+        const actual = substitutionModule.substitution(input, alphabet)
+        expect(actual).to.be.false
+})
+    it("Should return false if alphabet is not 26 characters", () => {
+        const input = "message"
+        const alphabet = "xoyqmcgrukswaflnthdjpzibev%"
+        const actual = substitutionModule.substitution(input, alphabet)
+        expect(actual).to.be.false
+})
+    it("Should return false if alphabet is missing", () => {
+        const input = "message"
+        const alphabet = "xoyqmcgrukswaflnthdjpzibev%"
+        const actual = substitutionModule.substitution(input)
+        expect(actual).to.be.false
+})
+})
